4f6fe8ca4229e1fe14ba546637d89c72ed73566d
TIEN NGUYEN <TIENQUOCNGUYEN22@GMAIL.com>
2017-02-25 19:39:14 -0800

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  root 'posts#index'
+  devise_for :users
+  devise_for :models
+  resources :posts
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
